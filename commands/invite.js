5f691415838a1b861b6669a65caec4afe2d0854b
Glitch (kyoukoos) <none>
2019-01-22 09:49:14 +0000

    🏪🐡 Updated with Glitch - [fixed]

--- commands/invite.js
+++ commands/invite.js
@@ -6,11 +6,8 @@ module.exports = {
     name: 'invite',
     description: 'invite link',
     cooldown: 5,
-    execute: async (message, args) => {
+    execute: async (client, message, args) => {
 message.author.send("https://bit.ly/2DcCwmq")
- .then(msg => {
-    msg.delete(4000)
-  })
-  .catch(/*Your Error handling if the Message isn't returned, sent, etc.*/);
+
 }}
 
