7d184f577f71f555e117cffe3c1429352a00a2d5
Glitch (kyoukooz) <none>
2019-01-22 12:52:06 +0000

    🎺🎄 Updated with Glitch

--- commands/invite.js
+++ commands/invite.js
@@ -7,7 +7,7 @@ module.exports = {
     description: 'invite link',
     cooldown: 5,
     execute: async (client, message, args) => {
-message.author.send("https://bit.ly/2DcCwmq")
+message.author.send("https://discordapp.com/api/oauth2/authorize?client_id=436093947358740480&permissions=8&scope=bot")
 
 }}
 
